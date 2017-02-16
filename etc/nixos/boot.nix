d21ce6b5129abb184a41f35beca4a74fedd46e89
lxk <lxk@droidcraft.org>
2017-02-16 20:05:59 +0300

    kernel and net hardening

--- etc/nixos/boot.nix
+++ etc/nixos/boot.nix
@@ -7,5 +7,25 @@
 {
 	#Additional boot options
 	boot.extraKernelParams = [ "pcie_port_pm=off" ];
+	boot.kernel.sysctl = {
+	    "vm.swappiness" = 0;
+	    "kernel.dmesg_restrict" = 1;
+	    #"kernel.kptr_restrict" = 1;
+
+	    #https://wiki.archlinux.org/index.php/Sysctl#TCP.2FIP_stack_hardening
+	    "net.ipv4.tcp_syncookies" = 1;
+	    "net.ipv4.tcp_rfc1337" = 1;
+	    "net.ipv4.conf.default.rp_filter" = 1;
+        "net.ipv4.conf.all.rp_filter" = 1;
+        "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
+        "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
+        "net.ipv4.conf.default.send_redirects" = 0;
+        "net.ipv4.conf.all.send_redirects" = 0;
+        "net.ipv4.conf.default.accept_redirects" = 0;
+        "net.ipv4.conf.all.accept_redirects" = 0;
+        "net.ipv6.conf.default.accept_redirects" = 0;
+        "net.ipv6.conf.all.accept_redirects" = 0;
+
+	};
 }
   
