05e5ca12bb270330ee37cdec7ce1d1c2cd244ff1
lxk <lxk@droidcraft.org>
2017-02-22 18:44:47 +0300

    fixed #19

--- etc/nixos/boot.nix
+++ etc/nixos/boot.nix
@@ -23,9 +23,9 @@
         "net.ipv4.conf.all.send_redirects" = 0;
         "net.ipv4.conf.default.accept_redirects" = 0;
         "net.ipv4.conf.all.accept_redirects" = 0;
-        "net.ipv6.conf.default.accept_redirects" = 0;
-        "net.ipv6.conf.all.accept_redirects" = 0;
-
+        "net.ipv6.conf.all.disable_ipv6" = 1;
+        "net.ipv6.conf.default.disable_ipv6" = 1;
+        "net.ipv6.conf.lo.disable_ipv6" = 1;
 	};
 }
   
